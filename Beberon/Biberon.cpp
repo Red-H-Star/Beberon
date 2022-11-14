15ed0268b58d7900b8ad3b347725e4a07305bc69
unknown <atruchon@gamingcampus.local>
2022-11-14 12:10:06 +0100

    Modif sur la classe Biberon

--- Beberon/Biberon.cpp
+++ Beberon/Biberon.cpp
@@ -9,17 +9,45 @@ protected:
 	int heure_prise;
 	bool vomi;
 public:
-	// Getters
-	int getQuantite_mini() {
-		return this->quantite_mini;
-	}
-	int getQuantite_restante() {
-		return this->quantite_restante;
-	}
-	int getQuantite_donnee() {
-		return this->quantite_donnee;
-	}
-	bool getVomi() {
-		return this->vomi;
-	}
+	Biberon();
+	Biberon(int, int, int, int, bool);
+	int getQuantite_mini();
+	int getQuantite_restante();
+	int getQuantite_donnee();
+	bool getVomi();
+	void setQuantite_restante(int);
+	void setQuantite_donnee(int);
 };
+
+// ============= Getters =============
+int Biberon::getQuantite_mini() {
+	return quantite_mini;
+}
+int Biberon::getQuantite_restante() {
+	return quantite_restante;
+}
+int Biberon::getQuantite_donnee() {
+	return quantite_donnee;
+}
+bool Biberon::getVomi() {
+	return vomi;
+}
+
+// ============= Setters =============
+void Biberon::setQuantite_restante(int newQuantite) {
+	quantite_restante = newQuantite;
+}
+void Biberon::setQuantite_donnee(int newQuantite) {
+	quantite_donnee = newQuantite;
+}
+
+// ============= Constructeur =============
+
+Biberon::Biberon() {
+	quantite_mini = 0;
+	quantite_restante = 0;
+	quantite_donnee = 0;
+	heure_prise = 0;
+	vomi = false;
+}
+
