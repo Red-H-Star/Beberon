e7036a86200f3a697950129c576af92b682c7f5f
unknown <atruchon@gamingcampus.local>
2022-11-17 11:09:36 +0100

    Modification effectué sur Biberon.h et Biberon.cpp

--- Beberon/Biberon.cpp
+++ Beberon/Biberon.cpp
@@ -1,12 +1,14 @@
 #include <iostream>;
 #include "Biberon.h";
+#include <string>;
+#include <ctime>;
 using namespace std;
 
 // ============= Initialisation =============
-void Biberon::fu(int delaiPrise, int heurePremierRepas) {
+void Biberon::creationPlanning(int heurePremierRepas, int delaiPrise) {
 	int nmbrRepas = getQuantite_max() - getQuantite_mini();
 	for (int i = 0; i < nmbrRepas; i++) {
-
+		_heure_prise[i] = (heurePremierRepas + delaiPrise)% 86400000;
 	}
 }
 
@@ -23,6 +25,12 @@ int Biberon::getQuantite_restante() {
 int Biberon::getQuantite_donnee() {
 	return _quantite_donnee;
 }
+int Biberon::getPremierePrise() {
+	return _premiere_prise;
+}
+int* Biberon::getHeure_Prise() {
+	return _heure_prise;
+}
 bool Biberon::getVomi() {
 	return _vomi;
 }
@@ -34,6 +42,9 @@ void Biberon::setQuantite_restante(int newQuantite) {
 void Biberon::setQuantite_donnee(int newQuantite) {
 	_quantite_donnee = newQuantite;
 }
+void Biberon::setPremierePrise(int premierePrise) {
+	_premiere_prise = premierePrise;
+}
 
 // ============= Constructeur =============
 
@@ -42,26 +53,30 @@ Biberon::Biberon() {																					// Constructeur par défaut de Biberon
 	_quantite_mini = 0;
 	_quantite_restante = 0;
 	_quantite_donnee = 0;
+	_premiere_prise = 0;
 	_heure_prise [0] = {};
 	_vomi = false;
 }
 
-Biberon::Biberon(int quantMax, int quantMini, int quantInitial, int intervallePrise) {								// Constructeur d'un nouveau biberon plein pas encore donné.
+Biberon::Biberon(int quantMax, int quantMini, int quantInitial											// Constructeur d'un nouveau biberon plein pas encore donné.
+				, int premierePrise, int intervallePrises) {
 	_quantite_max = quantMax;
 	_quantite_mini = quantMini;											
 	_quantite_restante = quantInitial;																	// Le biberon étant plein, la quantité restant = quantité initiale
 	_quantite_donnee = 0;																				// Vu que le biberon n'a pas encore était donné, quantité_donnée = 0
-	_heure_prise, intervallePrise;
+	_premiere_prise = premierePrise;
+	creationPlanning(_premiere_prise, intervallePrises);
 	_vomi = false;																						// Vu que le biberon n'a pas encore était donné, impossible qu'il ai été vomi
 }
 
 Biberon::Biberon(int quantiMax, int quantiMini, int quantiInital,										// Constructeur d'un biberon déjà donné
-				int quantiDonnee, int heureDonnee, bool vomito) {	
+				int quantiDonnee, int heureDonnee, int intervallePrises, bool vomito) {	
 	_quantite_max = quantiMax;
 	_quantite_mini = quantiMini;
 	_quantite_restante = quantiInital - quantiDonnee;													// Comme on a déjà donné le biberon au moins une fois, quantité restante = quantité intiale - quantité donnée
 	_quantite_donnee = quantiDonnee;
-	_heure_prise, heureDonnee;																			// Heure à laquelle on a donnée le biberon
+	_premiere_prise = heureDonnee;																		// Heure à laquelle on a donnée le biberon
+	creationPlanning(heureDonnee, intervallePrises);
 	_vomi = vomito;																						// On declare si le biberon a été vomi ou non
 }
 
@@ -70,3 +85,25 @@ Biberon::Biberon(int quantiMax, int quantiMini, int quantiInital,										// Co
 Biberon::~Biberon() {
 	
 }
+
+// ============= Méthodes =============
+
+void Biberon::afficherHoraire() {
+	string heureTraduite = "";
+
+}
+
+// ============= Choses à utiliser (prise de notes pour m'y retrouver) =============
+
+/*
+* -- Usage de la librairie ctime
+*	- // date / heure actuelle basée sur le système actuel
+		time_t tmm = time(0);
+	- double difftime (time_t time2, time_t time1);
+		Cette fonction calcule la différence en secondes entre time1 et time2.
+	
+	-- A faire
+		- afficherHoraire() qui affiche la prochaine prise de biberon
+		(trouvé comment faire la différence entre les heures dans le tableau et l'heure système et prendre la première occurence de temps supérieur à l'heure système)
+		- 
+*/
--- Beberon/Biberon.h
+++ Beberon/Biberon.h
@@ -1,23 +1,29 @@
 #pragma once
+
 class Biberon {
 protected:
 	int _quantite_max;
 	int _quantite_mini;
 	int _quantite_restante;
 	int _quantite_donnee;
+	int _premiere_prise;
 	int* _heure_prise;
 	bool _vomi;
 public:
 	Biberon();
-	Biberon(int, int, int, int);
-	Biberon(int, int, int, int, int, bool);
+	Biberon(int, int, int, int, int);
+	Biberon(int, int, int, int, int, int, bool);
 	~Biberon();
-	void fu(int, int);
+	void creationPlanning(int, int);
 	int getQuantite_max();
 	int getQuantite_mini();
 	int getQuantite_restante();
 	int getQuantite_donnee();
+	int getPremierePrise();
+	int* getHeure_Prise();
 	bool getVomi();
 	void setQuantite_restante(int);
 	void setQuantite_donnee(int);
+	void setPremierePrise(int);
+	void afficherHoraire();
 };
