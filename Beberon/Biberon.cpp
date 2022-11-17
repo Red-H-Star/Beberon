d0f5bd01986941cdb2b6766738c507f5a66b1aac
unknown <atruchon@gamingcampus.local>
2022-11-17 13:08:59 +0100

    Modification de Biberon et définition de la méthode afficher

--- Beberon/Biberon.cpp
+++ Beberon/Biberon.cpp
@@ -1,82 +1,70 @@
 #include <iostream>;
 #include "Biberon.h";
-#include <string>;
-#include <ctime>;
+#include <sstream>;
 using namespace std;
-
-// ============= Initialisation =============
-void Biberon::creationPlanning(int heurePremierRepas, int delaiPrise) {
-	int nmbrRepas = getQuantite_max() - getQuantite_mini();
-	for (int i = 0; i < nmbrRepas; i++) {
-		_heure_prise[i] = (heurePremierRepas + delaiPrise)% 86400000;
-	}
-}
 
 // ============= Getters =============
 int Biberon::getQuantite_max() {
 	return _quantite_max;
 }
-int Biberon::getQuantite_mini() {
-	return _quantite_mini;
+int Biberon::getQuantite_repas() {
+	return _quantite_repas;
 }
 int Biberon::getQuantite_restante() {
 	return _quantite_restante;
 }
-int Biberon::getQuantite_donnee() {
-	return _quantite_donnee;
-}
 int Biberon::getPremierePrise() {
 	return _premiere_prise;
 }
-int* Biberon::getHeure_Prise() {
-	return _heure_prise;
+int Biberon::getDelai_Prise() {
+	return _delai_prise;
 }
 bool Biberon::getVomi() {
 	return _vomi;
 }
 
 // ============= Setters =============
+void Biberon::setQuantite_repas(int newQuantite) {
+	_quantite_repas = newQuantite;
+}
 void Biberon::setQuantite_restante(int newQuantite) {
 	_quantite_restante = newQuantite;
 }
-void Biberon::setQuantite_donnee(int newQuantite) {
-	_quantite_donnee = newQuantite;
-}
 void Biberon::setPremierePrise(int premierePrise) {
 	_premiere_prise = premierePrise;
+}
+void Biberon::setDelai_Prise(int intervallePrise) {
+	_delai_prise = intervallePrise;
 }
 
 // ============= Constructeur =============
 
 Biberon::Biberon() {																					// Constructeur par défaut de Biberon
 	_quantite_max = 0;
-	_quantite_mini = 0;
+	_quantite_repas = 0;
 	_quantite_restante = 0;
-	_quantite_donnee = 0;
 	_premiere_prise = 0;
-	_heure_prise [0] = {};
+	_delai_prise = 0;
 	_vomi = false;
 }
 
-Biberon::Biberon(int quantMax, int quantMini, int quantInitial											// Constructeur d'un nouveau biberon plein pas encore donné.
-				, int premierePrise, int intervallePrises) {
+Biberon::Biberon(int quantMax, int quantRepas, int premierePrise										// Constructeur d'un nouveau biberon plein pas encore donné.
+				, int intervallePrises) {
 	_quantite_max = quantMax;
-	_quantite_mini = quantMini;											
-	_quantite_restante = quantInitial;																	// Le biberon étant plein, la quantité restant = quantité initiale
-	_quantite_donnee = 0;																				// Vu que le biberon n'a pas encore était donné, quantité_donnée = 0
+	_quantite_repas = quantRepas;											
+	_quantite_restante = _quantite_max;																	// Le biberon étant plein, la quantité restant = quantité max
 	_premiere_prise = premierePrise;
-	creationPlanning(_premiere_prise, intervallePrises);
+	_delai_prise = intervallePrises;
 	_vomi = false;																						// Vu que le biberon n'a pas encore était donné, impossible qu'il ai été vomi
 }
 
-Biberon::Biberon(int quantiMax, int quantiMini, int quantiInital,										// Constructeur d'un biberon déjà donné
-				int quantiDonnee, int heureDonnee, int intervallePrises, bool vomito) {	
+Biberon::Biberon(int quantiMax, int quantiRepas, int heureDonnee,										// Constructeur d'un biberon déjà donné
+				  int intervallePrises, bool vomito) {	
 	_quantite_max = quantiMax;
-	_quantite_mini = quantiMini;
-	_quantite_restante = quantiInital - quantiDonnee;													// Comme on a déjà donné le biberon au moins une fois, quantité restante = quantité intiale - quantité donnée
-	_quantite_donnee = quantiDonnee;
+	_quantite_repas = quantiRepas;
+	_quantite_restante = quantiMax - quantiRepas;														// Comme on a déjà donné le biberon une fois, quantité restante = quantité max - quantité repas
 	_premiere_prise = heureDonnee;																		// Heure à laquelle on a donnée le biberon
-	creationPlanning(heureDonnee, intervallePrises);
+	_delai_prise = intervallePrises;
 	_vomi = vomito;																						// On declare si le biberon a été vomi ou non
 }
 
@@ -87,23 +75,7 @@ Biberon::~Biberon() {
 }
 
 // ============= Méthodes =============
-
-void Biberon::afficherHoraire() {
-	string heureTraduite = "";
-
+string Biberon::afficherHoraire(time_t tempsSecondes) {													// Renvoie un string afin de pouvoir l'afficher dans l'application
+	tm* tempsLis = localtime(&tempsSecondes);
+	return "Le prochain biberon est prévu pour : " + to_string(1 + tempsLis->tm_hour) + ":" + to_string(1 + tempsLis->tm_min);
 }
-
-// ============= Choses à utiliser (prise de notes pour m'y retrouver) =============
-
-/*
-* -- Usage de la librairie ctime
-*	- // date / heure actuelle basée sur le système actuel
-		time_t tmm = time(0);
-	- double difftime (time_t time2, time_t time1);
-		Cette fonction calcule la différence en secondes entre time1 et time2.
-	
-	-- A faire
-		- afficherHoraire() qui affiche la prochaine prise de biberon
-		(trouvé comment faire la différence entre les heures dans le tableau et l'heure système et prendre la première occurence de temps supérieur à l'heure système)
-		- 
-*/
--- Beberon/Biberon.h
+++ Beberon/Biberon.h
@@ -1,29 +1,29 @@
 #pragma once
+#include <string>;
+#include <ctime>;
 
 class Biberon {
 protected:
 	int _quantite_max;
-	int _quantite_mini;
+	int _quantite_repas;
 	int _quantite_restante;
-	int _quantite_donnee;
 	int _premiere_prise;
-	int* _heure_prise;
+	int _delai_prise;
 	bool _vomi;
 public:
 	Biberon();
-	Biberon(int, int, int, int, int);
-	Biberon(int, int, int, int, int, int, bool);
+	Biberon(int, int, int, int);
+	Biberon(int, int, int, int, bool);
 	~Biberon();
-	void creationPlanning(int, int);
 	int getQuantite_max();
-	int getQuantite_mini();
+	int getQuantite_repas();
 	int getQuantite_restante();
-	int getQuantite_donnee();
 	int getPremierePrise();
-	int* getHeure_Prise();
+	int getDelai_Prise();
 	bool getVomi();
+	void setQuantite_repas(int);
 	void setQuantite_restante(int);
-	void setQuantite_donnee(int);
 	void setPremierePrise(int);
-	void afficherHoraire();
+	void setDelai_Prise(int);
+	string afficherHoraire(time_t);
 };
